38e39c40f05f6fbcc8212ab6c0f8ea9d4a1a989b
Jael Tan <jael.tan@graduateinstitute.ch>
2022-11-15 11:03:42 +0100

    Added COW IGO v3 dataset

---
+++ R/manyigos-organizations.R
@@ -0,0 +1,48 @@
+#' organizations database documentation
+#'
+#' @format The organizations database is a list that contains the
+#' following 1 datasets: COW.
+#' For more information and references to each of the datasets used,
+#' please use the `data_source()` and `data_contrast()` functions.
+#' @imports manydata
+#'\describe{
+#' \item{COW: }{A dataset with 16139 observations and the following
+#' 235 variables: igoID, Label, Beg, End, Year, afghanistan, albania, algeria,
+#' andorra, angola, antiguabarbuda, argentina, armenia, australia, austria,
+#' austriahungary, azerbaijan, baden, bahamas, bahrain, bangladesh, barbados,
+#' bavariaelect, belarus, belgium, belize, benin, bhutan, bolivia, bosnia,
+#' botswana, brazil, brunei, bulgaria, burkinafaso, burundi, car, cambodia,
+#' cameroon, canada, capeverde, chad, chile, china, colombia, comoros,
+#' congobrazz, costarica, croatia, cuba, cyprus, czechrep, czechoslovakia,
+#' denmark, djibouti, domrepublic, dominica, egermany, etimor, ecuador, egypt,
+#' elsalvador, eqguinea, eritrea, estonia, ethiopia, fyromacedonia, fiji,
+#' finland, france, gabon, gambia, georgia, germany, ghana, greece, grenada,
+#' guatemala, guinea, guineabissau, guyana, haiti, hanover, hesseelect,
+#' hessegrand, honduras, hungary, iceland, india, indonesia, iran, iraq,
+#' ireland, israel, italy, ivorycoast, jamaica, japan, jordan, kazakhstan,
+#' kenya, kiribati, korea, kosovo, kuwait, kyrgyzstan, laos, latvia, lebanon,
+#' lesotho, liberia, libya, liechtenstein, lithuania, luxembourg, madagascar,
+#' malawi, malaysia, maldives, mali, malta, marshallis, mauritania, mauritius,
+#' mecklenburg, mexico, micronesiafs, modena, moldova, monaco, mongolia,
+#' morocco, montenegro, mozambique, myanmar, namibia, nauru, nepal, netherlands,
+#' newzealand, nicaragua, niger, nigeria, nokorea, norway, nyemen, oman,
+#' pakistan, palau, panama, papalstates, papuanewguinea, paraguay, parma,
+#' peru, philippines, poland, portugal, qatar, romania, russia, rwanda,
+#' svietnam, syemen, sanmarino, saotome, saudiarabia, saxony, senegal,
+#' seychelles, sierraleone, singapore, slovakia, slovenia, soafrica,
+#' sokorea, sosudan, solomonis, somalia, spain, srilanka, stkitts,
+#' stlucia, stvincent, sudan, suriname, swaziland, sweden, switzerland,
+#' syria, taiwan, tajikistan, tanzania, thailand, togo, tonga, trinidad,
+#' tunisia, turkey, turkmenistan, tuscany, tuvalu, twosicilies, uae, uk,
+#' usa, uganda, ukraine, uruguay, uzbekistan, vanuatu, venezuela, vietnam,
+#' wgermany, wsamoa, wurtenburg, yemen, yugoslaviaserb, zaire, zambia,
+#' zanzibar, zimbabwe, orgname, dead, integrated, replaced, ionum, igocode,
+#' version, accuracyofpre1965membershipdates, sourcesandnotes, imputed,
+#' political, social, economic.}
+#' }
+#'
+#' @details
+#' ``` {r, echo = FALSE, warning = FALSE}
+#' lapply(organizations, messydates::mreport)
+#' ```
+"organizations"
