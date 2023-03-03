74cdb4d5c62ca6c1a489c27990cac82e7cde1e9c
Jael Tan <jael.tan@graduateinstitute.ch>
2023-03-03 11:33:48 +0100

    Added first version of memberships data

---
+++ tests/testthat/test_COW_MEM.R
@@ -0,0 +1,50 @@
+# Test if the dataset meets the many packages universe requirements
+
+# Report missing values
+test_that("missing observations are reported correctly", {
+  expect_false(any(grepl("^n/a$", memberships[["COW_MEM"]])))
+  expect_false(any(grepl("^N/A$", memberships[["COW_MEM"]])))
+  expect_false(any(grepl("^\\s$", memberships[["COW_MEM"]])))
+  expect_false(any(grepl("^\\.$", memberships[["COW_MEM"]])))
+  expect_false(any(grepl("N\\.A\\.$", memberships[["COW_MEM"]])))
+  expect_false(any(grepl("n\\.a\\.$", memberships[["COW_MEM"]])))
+})
+
+# Uniformity tests (agreements have a countryID and Beg columns)
+test_that("datasets have the required variables", {
+  pointblank::expect_col_exists(memberships[["COW_MEM"]],
+                                pointblank::vars(stateID))
+  pointblank::expect_col_exists(memberships[["COW_MEM"]],
+                                pointblank::vars(Beg))
+})
+
+# Date columns should be in mdate class
+test_that("Columns are not in date, POSIXct or POSIXlt class", {
+  expect_false(any(lubridate::is.Date(memberships[["COW_MEM"]])))
+  expect_false(any(lubridate::is.POSIXct(memberships[["COW_MEM"]])))
+  expect_false(any(lubridate::is.POSIXlt(memberships[["COW_MEM"]])))
+})
+
+# Dates are standardized for mandatory column
+test_that("Column `Beg` has standardised dates", {
+  expect_equal(class(memberships[["COW_MEM"]]$Beg), "mdate")
+  expect_false(any(grepl("/", memberships[["COW_MEM"]]$Beg)))
+  expect_false(any(grepl("^[:alpha:]$",
+                         memberships[["COW_MEM"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{2}$",
+                         memberships[["COW_MEM"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{3}$",
+                         memberships[["COW_MEM"]]$Beg)))
+  expect_false(any(grepl("^[:digit:]{1}$",
+                         memberships[["COW_MEM"]]$Beg)))
+})
+
+# Dataset should be ordered according to the "Beg" column
+test_that("dataset is arranged by the `Beg` variable", {
+  expect_true(memberships[["COW_MEM"]]$Beg[1] <
+                memberships[["COW_MEM"]]$Beg[100])
+  expect_true(memberships[["COW_MEM"]]$Beg[120] <
+                memberships[["COW_MEM"]]$Beg[220])
+  expect_true(memberships[["COW_MEM"]]$Beg[250] <
+                memberships[["COW_MEM"]]$Beg[350])
+})
