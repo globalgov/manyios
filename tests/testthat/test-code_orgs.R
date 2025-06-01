2ac240aea7d8f3ca6323f8ca231f4014c405def7
James Hollway <james.hollway@graduateinstitute.ch>
2025-06-01 17:44:20 +0200

    Test for org codes being upper case

--- tests/testthat/test-code_orgs.R
+++ tests/testthat/test-code_orgs.R
@@ -2,6 +2,10 @@ orgs <- code_orgs() %>% dplyr::filter(nchar(orgID) > 5 | nchar(orgID) < 4) %>%
   dplyr::pull(orgID)
 
 
+test_that("org codes are all upper case", {
+  expect_equal(code_orgs()$orgID, toupper(code_orgs()$orgID))
+})
+
 test_that("org codes are unique", {
   expect_false(any(duplicated(orgRegex$orgID)))
 })
